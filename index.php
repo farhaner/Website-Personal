ffd45756d3ec0b01c9f6d231161a9d7a2245eafe
farhaner <132831638+farhaner@users.noreply.github.com>
2023-05-19 23:08:36 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="css/stlye.css">
+  <title>Filosoficoffee</title>
+</head>
+
+<body class="bg-dark">
+
+  <nav class="navbar navbar-expand-lg bg-dark navbar-dark fixed-top border-bottom">
+    <div class="container">
+      <a class="navbar-brand text-decoration-none text-white fw-bold fw-italic" href="#">Kenangan<span>Senja</span> </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup center">
+        <div class="navbar-nav">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+          <a class="nav-link" href="#about">About</a>
+          <a class="nav-link" href="#menu">Menu</a>
+          <a class="nav-link" href="#contact">Kontak</a>
+        </div>
+        <div class="ms-auto">
+          <a href="" class="text-decoration-none text-white"><i class="bi bi-box-arrow-in-right text-white">Login</i></a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <section class="hero" id="home">
+    <main class="content">
+      <div class="fw-bold">
+        <h1>Bersantai Dengan Nikmatnya Kopi</h1>
+      </div>
+      <div class="col-md-6 fw-light">
+        <p>Secangkir kopi dapat mengurangi rasa lelah sehingga bisa menambah semangat. Kopi dapat membantu kita melewati hari yang panjang, membuat merasa hangat di dalam sehingga kita bisa bersikap baik kepada orang-orang.</p>
+      </div>
+    </main>
+  </section>
+
+  <section class="about  " id="about">
+    <div class=" text-center text-white">
+      <h2><span>Tentang</span>Kami</h2>
+      <div class="img mt-5">
+        <img src="img/gambar-2.jpg" alt="">
+      </div>
+      <div class="row justify-content-center mt-3">
+        <div class="col-md-3 fw-light">
+          <p>filosoficoffee? merupakan tempatnya semua kalangan kalangan berawal dari remaja, dewasa sampai yang tua menikmati kopi dengan santai dan syahdu</p>
+        </div>
+        <div class="col-md-3 fw-light">
+          <p>kopi juga merupakan satu di antara bahan minuman yang memiliki banyak penggemar di seluruh penjuru dunia. Di mana pun kopi dinikmati, kantor, rumah, kedai, kafe, warung di pinggir jalan, tak akan mengurangi kenikmatan dari secangkir kopi.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="menu" id="menu">
+    <div class="menu text-center text-white">
+      <h2><span>Menu</span>Kami</h2>
+      <div class="row justify-content-center mt-4">
+        <div class="text-center col-4">
+          <img src="img/gambar-6.jpg" alt="menu-1" width="100" class="rounded-circle mt-md-3">
+          <p>Macchiato Coffee</p>
+          <p>30k</p>
+        </div>
+        <div class="text-center col-4">
+          <img src="img/gambar-6.jpg" alt="menu-1" width="100" class="rounded-circle mt-md-3">
+          <p>Latte Coffee</p>
+          <p>30k</p>
+        </div>
+        <div class="text-center col-4">
+          <img src="img/gambar-6.jpg" alt="menu-1" width="100" class="rounded-circle mt-md-3">
+          <p>cappucino Coffee</p>
+          <p>32k</p>
+        </div>
+        <div class="text-center col-4">
+          <img src="img/gambar-6.jpg" alt="menu-1" width="100" class="rounded-circle mt-md-3">
+          <p>Esspreso Coffee</p>
+          <p>25k</p>
+        </div>
+        <div class="text-center col-4">
+          <img src="img/gambar-6.jpg" alt="menu-1" width="100" class="rounded-circle mt-md-3">
+          <p>Frapucino Coffee</p>
+          <p>30k</p>
+        </div>
+        <div class="text-center col-4">
+          <img src="img/gambar-6.jpg" alt="menu-1" width="100" class="rounded-circle mt-md-3">
+          <p>Americano Coffee</p>
+          <p>33k</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="contact " id="contact">
+    <div class=" text-center text-white">
+      <h2><span>Kontak</span>Kami</h2>
+      <footer class="mt-5">
+        <a href="instagram.com/fan.er23"><i class="bi bi-instagram text-white"></i></a>
+        <a href=""><i class="bi bi-whatsapp text-white"></i></a>
+        <a href=""><i class="bi bi-twitter text-white"></i></a>
+        <p>Website created by <a href="" class="text-decoration-none">farhan erlangga</a></p>
+      </footer>
+    </div>
+  </section>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
